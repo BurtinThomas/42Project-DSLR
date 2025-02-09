19fad037bd1ec5cc655000dd4626ad24626f1fd0
Francois <transfo@Francoiss-MacBook-Air.local>
2025-02-09 13:45:16 +0100

    rvv

---
+++ visualisation/pair_plot.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+def pair_plot(df):
+    sns.pairplot(df, hue='Hogwarts House')
+    plt.show()
+
+def main():
+    try:
+        df = pd.read_csv('../datasets/dataset_train.csv')
+        pair_plot(df)
+    except Exception as error:
+        print(f"{type(error).__name__} : {error}")
+    except KeyboardInterrupt:
+        print("\nInput interrupted. Exiting...")
+
+if __name__ == "__main__":
+    main()
