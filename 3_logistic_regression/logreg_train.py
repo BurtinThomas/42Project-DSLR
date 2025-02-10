d14a58c62adc6bb944c98e7fe5f70e76cb954a15
Samuel_Guillot <samuelguillot75@gmail.com>
2025-02-10 16:48:33 +0100

    all training

--- 3_logistic_regression/logreg_train.py
+++ 3_logistic_regression/logreg_train.py
@@ -4,6 +4,7 @@ import numpy as np
 
 
 DATASETS_LOCATION = "../datasets/"
+WEIGHTS_LOCATION = "../weights/"
 LEARNING_RATE = 0.01
 NUM_ITERATIONS = 1000
 
@@ -12,38 +13,74 @@ class Training:
     def __init__(self, dataset):
         self.dataset = dataset
         self.weights = {}
-        
+        self.x = None
+        self.y = None
+        self.cost_history = []
+
         self.load_data()
         self.preprocess_data()
         self.train()
         self.save_weights()
-        
-    
+
+
     def load_data(self):
         try:
             data = pd.read_csv(self.dataset)
-            # ?
+            data = data.dropna()
+            self.x = np.array((data.iloc[:,5:]))
+            self.y = np.array(data.loc[:,"Hogwarts House"])
         except Exception as error:
             raise RuntimeError(f"Error loading data: {error}")
-        
+
+
     def preprocess_data(self):
-        pass # ?
-        
+        self.x = (self.x - np.mean(self.x, axis=0)) / (np.std(self.x, axis=0) + 1e-8)
+
+        unique_labels = np.unique(self.y)
+        self.label_mapping = {label: id for id, label in enumerate(unique_labels)}
+        self.y = np.array([self.label_mapping[label] for label in self.y])
+
 
     def sigmoid(self, z):
         return 1 / (1 + np.exp(-z))
-        
+
+
     def compute_cost(self, x, y, theta):
-        pass # ?
+        m = len(y)
+        predictions = self.sigmoid(np.dot(x, theta))
+        cost = (-1 / m) * np.sum(
+            y * np.log(predictions + 1e-8) + 
+            (1 - y) * np.log(1 - predictions + 1e-8)
+        )
+        return cost
+
 
     def gradient_descent(self, x, y):
-        pass # ?
+        m, n = x.shape
+        theta = np.zeros(n)
+        for i in range(NUM_ITERATIONS):
+            predictions = self.sigmoid(np.dot(x, theta))
+            gradient = np.dot(x.T, (predictions - y)) / m # T => matrice x transposition
+            theta -= LEARNING_RATE * gradient
+            
+            cost = self.compute_cost(self.x, self.y, theta)
+            self.cost_history.append(cost)
+            
+        return theta
+
     
     def train(self):
-        pass #?
+        # bias = np.ones((self.x.shape[0], 1))
+        # self.x = np.hstack((bias, self.x))
+        
+        unique_labels = np.unique(self.y)
+        for label in unique_labels:
+            y_binary = (self.y == label).astype(int)
+            self.weights[label] = self.gradient_descent(self.x, y_binary)
+
     
     def save_weights(self):
-        pass #?
+        np.save(f"{WEIGHTS_LOCATION}weights.npy", self.weights)
 
 
 def main():
